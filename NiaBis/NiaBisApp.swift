c3d1a847c0bac49f4674c13422cd0e38bfdd39dc
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-02-22 00:35:21 +0900

    initial Project

--- NiaBis/NiaBisApp.swift
+++ NiaBis/NiaBisApp.swift
@@ -1,32 +1,11 @@
-//
-//  NiaBisApp.swift
-//  NiaBis
-//
-//  Created by zunda on 2024/02/22.
-//
-
 import SwiftUI
 import SwiftData
 
 @main
 struct NiaBisApp: App {
-    var sharedModelContainer: ModelContainer = {
-        let schema = Schema([
-            Item.self,
-        ])
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
-        do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
-        } catch {
-            fatalError("Could not create ModelContainer: \(error)")
-        }
-    }()
-
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-        .modelContainer(sharedModelContainer)
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
