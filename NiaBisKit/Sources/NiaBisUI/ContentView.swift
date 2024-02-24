889cdf57dd72ad6b4a2f0510f06e29819934d77e
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-02-25 04:42:44 +0900

    add ContentView, SignInOrSignUpView

--- NiaBisKit/Sources/NiaBisUI/ContentView.swift
+++ NiaBisKit/Sources/NiaBisUI/ContentView.swift
@@ -1,18 +1,59 @@
 import SwiftUI
 
 public struct ContentView: View {
+  @State var router = NavigationRouter()
+  let authController = AuthController()
+  let errorController = ErrorController()
+  
   public init() {
     
   }
   
+  @ViewBuilder
+  var content: some View {
+    if let session = authController.session {
+      VStack {
+        Text(session.user.id.uuidString)
+      }
+        .environment(\.session, session)
+        .toolbar {
+          ToolbarItem(placement: .topBarLeading) {
+            Button("SignOut") {
+              Task(priority: .high) {
+                do {
+                  try await supabase.auth.signOut()
+                } catch {
+                  errorController.error = error
+                }
+              }
+            }
+          }
+        }
+    } else {
+      StartView()
+    }
+  }
+  
   public var body: some View {
-    NavigationStack {
-      Text("ContentView")
+    NavigationStack(path: $router.routes) {
+      content
+        .navigationDestination(for: NavigationRouter.Item.self) { item in
+          switch item {
+          case .signIn:
+            SignInOrSignUpView(mode: .signIn)
+          case .signUp:
+            SignInOrSignUpView(mode: .signUp)
+          }
+        }
         .navigationTitle("NiaBis")
     }
+    .environment(router)
+    .environment(authController)
+    .environment(errorController)
   }
 }
 
 #Preview {
   ContentView()
 }
+
---
+++ NiaBisKit/Sources/NiaBisUI/SignInOrSignUpView.swift
@@ -0,0 +1,184 @@
+import SwiftUI
+import Auth
+import AuthenticationServices
+
+struct SignInOrSignUpView: View {
+  enum Mode {
+    case signIn
+    case signUp
+  }
+  
+  let mode: Mode
+  
+  @Environment(\.webAuthenticationSession) var webAuthenticationSession
+  @Environment(AuthController.self) var authController
+  @State var error: IdentifiedItem<Error>?
+  @Environment(\.openURL) var openURL
+
+  @State var email: String = ""
+  @State var password: String = ""
+  @State var isPresentedConfirmMail = false
+  
+  func login(provider: Provider) async {
+    do {
+      let signInURL = try await supabase.auth.getOAuthSignInURL(
+        provider: provider,
+        redirectTo: Constants.redirectToURL
+      )
+      
+      let urlWithToken = try await webAuthenticationSession.authenticate(
+        using: signInURL,
+        callbackURLScheme: Constants.redirectToURL.scheme!
+      )
+      
+      try await supabase.auth.session(from: urlWithToken)
+    } catch {
+      self.error = .init(item: error)
+    }
+  }
+  
+  func siginIn(with email: String, password: String) async {
+    guard !email.isEmpty && !password.isEmpty else { return }
+
+    do {
+      let newSession = try await supabase.auth.signIn(
+        email: email,
+        password: password
+      )
+      
+      authController.session = newSession
+    } catch {
+      self.error = .init(item: error)
+    }
+  }
+  
+  func siginUp(with email: String, password: String) async {
+    guard !email.isEmpty && !password.isEmpty else { return }
+
+    do {
+      try await supabase.auth.signUp(
+        email: email,
+        password: password,
+        redirectTo: Constants.redirectToURL
+      )
+      
+      isPresentedConfirmMail.toggle()
+    } catch {
+      self.error = .init(item: error)
+    }
+  }
+  
+  func resendConfirmMail(email: String) async {
+    do {
+      try await supabase.auth.resend(email: email, type: .signup)
+    } catch {
+      self.error = .init(item: error)
+    }
+  }
+  
+  func openMailApp() {
+    let mailAppURL = URL(string: "message://")!
+    openURL(mailAppURL)
+  }
+  
+  func onOpenURLVerifySetSession(url: URL) async {
+    do {
+      let session = try await supabase.auth.session(from: url)
+      authController.session = session
+    } catch {
+      self.error = .init(item: error)
+    }
+  }
+  
+  var body: some View {
+    Form {
+      if let session = authController.session {
+        Section {
+          Text(session.user.email!)
+        }
+      }
+      
+      Section {
+        TextField("Email", text: $email)
+          .textContentType(.emailAddress)
+          .keyboardType(.emailAddress)
+          .autocorrectionDisabled()
+          .textInputAutocapitalization(.never)
+
+        SecureField("Password", text: $password)
+          .textContentType(mode == .signIn ? .password : .newPassword)
+          .autocorrectionDisabled()
+          .textInputAutocapitalization(.never)
+                
+        switch mode {
+        case.signIn:
+          Button("Sign In with Email") {
+            Task(priority: .high) {
+              await siginIn(with: email, password: password)
+            }
+          }
+        case .signUp:
+          Button("Sign up with Email") {
+            Task(priority: .high) {
+              await siginUp(with: email, password: password)
+            }
+          }
+        }
+      }
+      
+      if isPresentedConfirmMail {
+        Button("Resend Mail") {
+          Task(priority: .high) {
+            await resendConfirmMail(email: email)
+          }
+        }
+      }
+      
+      if let error {
+        Section {
+          if let error = error.item as? LocalizedError {
+            if let failureReason = error.failureReason {
+              Text(failureReason)
+            }
+            if let recoverySuggestion = error.recoverySuggestion {
+              Text(recoverySuggestion)
+            }
+          } else {
+            Text(error.item.localizedDescription)
+          }
+        }
+      }
+      
+      Button {
+        Task(priority: .high) {
+          await login(provider: .google)
+        }
+      } label: {
+        Text("Sin in with Google")
+      }
+      .buttonBorderShape(.roundedRectangle)
+    }
+    .formStyle(.grouped)
+    .onSubmit(of: .text) {
+      Task(priority: .high) {
+        await siginIn(with: email, password: password)
+      }
+    }
+    .alert("Confirm Mail", isPresented: $isPresentedConfirmMail) {
+      Button("Open Mail") {
+        openMailApp()
+      }
+    }
+    .onOpenURL { url in
+      Task(priority: .high) {
+        await onOpenURLVerifySetSession(url: url)
+      }
+    }
+  }
+}
+
+#Preview {
+  SignInOrSignUpView(mode: .signIn)
+    .environment(AuthController())
+    .environment(ErrorController())
+}
