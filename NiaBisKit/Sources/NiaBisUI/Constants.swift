abc6804e3dc78cd2e5a8305171568106c5209214
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-02-25 04:40:34 +0900

    add appGroup, appIdentifierPrefix, bundleIdentifier

--- NiaBisKit/Sources/NiaBisUI/Constants.swift
+++ NiaBisKit/Sources/NiaBisUI/Constants.swift
@@ -2,4 +2,14 @@ import Foundation
 
 enum Constants {
   static let redirectToURL = URL(string: "niabis://")!
+  static public let appGroup = "group.zunda.niabis"
+  static var appIdentifierPrefix: String {
+    var appIdentifierPrefix = Bundle.main.infoDictionary!["AppIdentifierPrefix"] as! String
+    if appIdentifierPrefix.last == "." {
+      appIdentifierPrefix.removeLast()  // remove [.] dot
+    }
+    return appIdentifierPrefix
+  }
+  
+  static var bundleIdentifier: String = Bundle.main.infoDictionary!["CFBundleIdentifier"] as! String
 }
