43b7c46bcf495cb1bdb663b707cd8f73ef35b4fc
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-05-05 01:59:25 +0900

    add generate env code shell

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,3 +2,15 @@
 
 defaults write com.apple.dt.Xcode IDESkipPackagePluginFingerprintValidatation -bool YES
 defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+
+env_file=".env"
+touch $env_file
+
+cat > $env_file <<EOL
+niabisAPIToken=${NIABIS_API_TOKEN}
+cloudflareImagesAccountHashId=${CLOUDFLARE_IMAGES_ACCOUNT_HASH_ID}
+EOL
+
+cd NiaBisKit
+
+swift package plugin --allow-writing-to-directory Sources generate-env-code SecretConstants ../${env_file} Sources/SecretConstants.swift
