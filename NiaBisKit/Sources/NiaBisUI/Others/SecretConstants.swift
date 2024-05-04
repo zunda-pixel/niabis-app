cd9209847b08869cc07770d04c5cfc2c0f66f113
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-05-05 01:50:59 +0900

    add SecretConstants

---
+++ NiaBisKit/Sources/NiaBisUI/Others/SecretConstants.swift
@@ -0,0 +1,4 @@
+public enum SecretConstants {
+  static public var cloudflareImagesAccountHashId: String = <#CLOUDFLARE_IMAGES_ACCOUNT_HASH_ID#>
+  static public var niabisAPIToken: String = <#NIABIA_API_TOKEN#>
+}
