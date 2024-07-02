1962500f21416cb927e6fd198addaffd4ae542f3
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-07-02 23:48:25 +0900

    update: Update library (#24)
    * update library
    
    * remove swift-format
    
    * update swift-version
    
    * add @retroactive

--- NiaBisKit/Package.swift
+++ NiaBisKit/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -16,16 +16,15 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/supabase-community/supabase-swift", from: "2.1.3"),
+    .package(url: "https://github.com/supabase-community/supabase-swift", from: "2.13.7"),
     .package(url: "https://github.com/apple/swift-algorithms", from: "1.2.0"),
-    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.0.0"),
-    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.3.0"),
-    .package(url: "https://github.com/apple/swift-format", from: "509.0.0"),
-    .package(url: "https://github.com/kean/Nuke", from: "12.4.0"),
+    .package(url: "https://github.com/pointfreeco/swift-identified-collections", from: "1.1.0"),
+    .package(url: "https://github.com/pointfreeco/swift-case-paths", from: "1.4.2"),
+    .package(url: "https://github.com/kean/Nuke", from: "12.7.3"),
     .package(url: "https://github.com/zunda-pixel/LicenseProvider", from: "1.1.2"),
-    .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.1.3"),
-    .package(url: "https://github.com/apple/swift-http-types", from: "1.0.0"),
-    .package(url: "https://github.com/danielsaidi/SystemNotification", from: "1.1.0"),
+    .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.2.0"),
+    .package(url: "https://github.com/apple/swift-http-types", from: "1.2.0"),
+    .package(url: "https://github.com/danielsaidi/SystemNotification", from: "1.1.1"),
     .package(url: "https://github.com/zunda-pixel/selene", from: "1.2.3"),
   ],
   targets: [
