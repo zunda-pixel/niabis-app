ff53b73a8b7c727d5efdd0d4ff8a9b269811392d
zunda <47569369+zunda-pixel@users.noreply.github.com>
2024-08-10 15:58:39 +0900

    revive LicenseProvider

--- NiaBisKit/Sources/NiaBisUI/Settings/LicensesView.swift
+++ NiaBisKit/Sources/NiaBisUI/Settings/LicensesView.swift
@@ -3,20 +3,19 @@ import SwiftUI
 struct LicensesView: View {
   var body: some View {
     List {
-      Text("Licenses")
-//      ForEach(LicenseProvider.packages) { package in
-//        NavigationLink(package.name) {
-//          ScrollView {
-//            Text(package.license)
-//          }
-//          .navigationTitle(package.name)
-//          .toolbar {
-//            ToolbarItem {
-//              ShareLink(item: package.location)
-//            }
-//          }
-//        }
-//      }
+      ForEach(LicenseProvider.packages) { package in
+        NavigationLink(package.name) {
+          ScrollView {
+            Text(package.license)
+          }
+          .navigationTitle(package.name)
+          .toolbar {
+            ToolbarItem {
+              ShareLink(item: package.location)
+            }
+          }
+        }
+      }
     }
     .navigationTitle(Text("Licenses", bundle: .module))
   }
